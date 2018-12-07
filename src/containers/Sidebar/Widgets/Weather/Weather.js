894ec771d478e17e1b086ba4d5e8c20495253e1f
Daniel Jimenez <Daniel@CORP.EXACTSTAFF.COM>
2018-12-07 13:27:48 -0800

    updating the weather component

--- src/containers/Sidebar/Widgets/Weather/Weather.js
+++ src/containers/Sidebar/Widgets/Weather/Weather.js
@@ -2,11 +2,24 @@ import React,{Component} from 'react';
 import classes from './Weather.css';
 
 class Weather extends Component {
+    
+    
     render() {
         return(
         <div className={classes.Weather}>
             {/* <iframe tittle="weather widget" id="forecast_embed" frameBorder="0" height="245" width="100%" src="//forecast.io/embed/#lat=34.1672543&color=#00aaff&lon=-118.5939473&name=Woodland Hills"></iframe> */}
-            <a className="weatherwidget-io" href="https://forecast7.com/en/34d05n118d24/los-angeles/?unit=us" data-label_1="LOS ANGELES" data-label_2="WEATHER" data-icons="Climacons Animated" data-theme="weather_one" >LOS ANGELES WEATHER</a>
+            <div className="container">
+                <div className="header">
+                    <h3>Los Angeles, CA</h3>
+                    {/* <h1><WiDaySunny size={80} color='#000' /></h1> */}
+                    {/* <h2><Clock format={'HH:mm'} ticking={true} timezone={'US/Pacific'} /> AM</h2> */}
+                </div>
+                <div className="weather">
+                    {/* <h1 className="temp">{this.state.weather.apparentTemperature} &#8457;</h1> */}
+                    <p>Humidity: {this.state.weather.humidity} %</p>
+                    <p>Wind: {this.state.weather.windGust} mph</p>
+                </div>
+            </div>
         </div>
         );
     }
