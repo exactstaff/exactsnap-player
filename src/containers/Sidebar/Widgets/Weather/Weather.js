03d1826fe7c8a72e43a19ccde60dce70f28f2df1
Daniel Jimenez <Daniel@CORP.EXACTSTAFF.COM>
2018-12-21 11:17:21 -0800

    working on the style for the amazing weather widget

--- src/containers/Sidebar/Widgets/Weather/Weather.js
+++ src/containers/Sidebar/Widgets/Weather/Weather.js
@@ -27,8 +27,7 @@ class Weather extends Component {
         }
     }
 
-
-    componentDidMount(){
+    getWeather() {
         axios.get('http://api.ipstack.com/45.26.254.145?access_key=8773b56636e9c17e2e130a4329d4bf9c', {'crossDomain': true})
         .then((location) => {
             console.log(location);
@@ -54,9 +53,9 @@ class Weather extends Component {
             let icon = response.data.currently.icon;
 
             let currentWeather = {
-                apparentTemperature: apparentTemperature,
+                apparentTemperature: Math.round(apparentTemperature * 10 ) / 10,
                 humidity: Math.round(humidity * 100),
-                windGust: windGust,
+                windGust: Math.round((windGust * 2.237) * 10 ) / 10,
                 summary: summary,
                 icon: icon
             };
@@ -72,28 +71,46 @@ class Weather extends Component {
         this.setState(
             {location: myLocation}
         );
-    })
-}
+
+        // setInterval(function(){
+        //     window.location.reload(1);
+        //  }, 10000);
+    });
+    }
+
+    componentDidMount(){
+       this.getWeather();
+       setInterval(()=>{
+            this.getWeather();
+            console.log("Weather refreshed");
+        }, 10000);
+    }
 
     render() {
         return(
         <div className={classes.Weather}>
+
             <div className={[classes.container, backgroundChanger(this.state.weather.icon)].join(' ')} >
-            <h3>{this.state.location.city}, {this.state.location.region}</h3>
-            <p>{this.state.weather.summary}</p>
 
-                    <img className={classes.icon} src={iconChanger(this.state.weather.icon)}  alt="icons" /> 
-
-                    <div className={classes.column_one}>
-                        <h1 className="temp">{this.state.weather.apparentTemperature} F</h1>
-                    </div>
-
-                    <div className={classes.column_two}>
-                        {/* <h3><Clock format={'h:mm a'} timezone={'US/Pacific'}/></h3> */}
-                        <p>Humidity: {this.state.weather.humidity} %</p>
-                        <p>Wind: {this.state.weather.windGust} mph</p>
-                    </div>
+                <div className={classes.column_three}>
+                    <h3>{this.state.location.city}, {this.state.location.region}</h3>
+                </div>
                 
+                <div className={classes.column_four}>
+                    <h3>{this.state.weather.summary}</h3>
+                </div>
+
+                <img className={classes.icon} src={iconChanger(this.state.weather.icon)}  alt="icons" /> 
+
+
+                <div className={classes.column_one}>
+                    <p className={classes.temp}>{this.state.weather.apparentTemperature} F</p>
+                </div>
+
+                <div className={classes.column_two}>                
+                    <p>Humidity: {this.state.weather.humidity}%</p>
+                    <p>Wind: {this.state.weather.windGust} mph</p>
+                </div>    
             </div>
         </div>
         );
@@ -103,3 +120,4 @@ class Weather extends Component {
 export default Weather;
 
 // SET HUMIDITY TO MPH CURRENTLY IN KHM
+//<h3><Clock format={'h:mm a'} timezone={'US/Pacific'}/></h3>  //Commented out the import for the clock
