f90d97e61e3a1f3b24719325d9e7ffbdfd7461cd
Kervin Vasquez <kvasquez@exactstaff.com>
2020-07-10 13:18:44 -0700

    change header

--- src/lib/weather.js
+++ src/lib/weather.js
@@ -24,7 +24,7 @@ export default async function get() {
       {
         crossDomain: true,
         headers: {
-          origin: "https://www.exactstaff.com",
+          "x-requested-with": "XMLHttpRequest",
         },
       }
     );
